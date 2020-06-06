6a5d01ca8ff55d552c1852b6fa3ce718ce5e7784
Kai Chen <xmkevinchen@gmail.com>
2020-06-05 23:57:14 -0700

    Google inject.dart working sample

--- lib/src/di/app_injector.dart
+++ lib/src/di/app_injector.dart
@@ -1,15 +1,24 @@
 
 import 'package:inject/inject.dart';
 
+import '../../main.dart';
 import '../domain/repository.dart';
+import '../ui/weather_screen.dart';
+import 'app_injector.inject.dart' as g;
 
 @Injector()
 abstract class AppInjector {
 
   @provide
+  MyApp get app;
+
+  @provide
   Repository get repository;
 
+  @provide
+  WeatherScreen get weatherScreen;
+
   static Future<AppInjector> create() {
-    
+    return g.AppInjector$Injector.create();
   }
 }
